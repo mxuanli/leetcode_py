976fbfbb235b586fd471492439e9b9334ebc2b1b
hebohao <hebohao@bfops.cn>
2021-05-21 15:25:41 +0800

    692、1035

---
+++ 692. 前K个高频单词.py
@@ -0,0 +1,20 @@
+from collections import defaultdict, Counter
+
+
+class Solution:
+    def topKFrequent(self, words: list, k: int) -> list:
+        c = Counter(words)
+        words_dict = defaultdict(list)
+        for key, v in c.items():
+            words_dict[v].append(key)
+        r = list()
+        for key in sorted(words_dict.keys(), reverse=True):
+            r += sorted(words_dict[key])
+        return r[:k]
+
+
+s = Solution()
+words = ["plpaboutit","jnoqzdute","sfvkdqf","mjc","nkpllqzjzp","foqqenbey","ssnanizsav","nkpllqzjzp","sfvkdqf","isnjmy","pnqsz","hhqpvvt","fvvdtpnzx","jkqonvenhx","cyxwlef","hhqpvvt","fvvdtpnzx","plpaboutit","sfvkdqf","mjc","fvvdtpnzx","bwumsj","foqqenbey","isnjmy","nkpllqzjzp","hhqpvvt","foqqenbey","fvvdtpnzx","bwumsj","hhqpvvt","fvvdtpnzx","jkqonvenhx","jnoqzdute","foqqenbey","jnoqzdute","foqqenbey","hhqpvvt","ssnanizsav","mjc","foqqenbey","bwumsj","ssnanizsav","fvvdtpnzx","nkpllqzjzp","jkqonvenhx","hhqpvvt","mjc","isnjmy","bwumsj","pnqsz","hhqpvvt","nkpllqzjzp","jnoqzdute","pnqsz","nkpllqzjzp","jnoqzdute","foqqenbey","nkpllqzjzp","hhqpvvt","fvvdtpnzx","plpaboutit","jnoqzdute","sfvkdqf","fvvdtpnzx","jkqonvenhx","jnoqzdute","nkpllqzjzp","jnoqzdute","fvvdtpnzx","jkqonvenhx","hhqpvvt","isnjmy","jkqonvenhx","ssnanizsav","jnoqzdute","jkqonvenhx","fvvdtpnzx","hhqpvvt","bwumsj","nkpllqzjzp","bwumsj","jkqonvenhx","jnoqzdute","pnqsz","foqqenbey","sfvkdqf","sfvkdqf"]
+k = 1
+r = s.topKFrequent(words, k)
+print(r)
