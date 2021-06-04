f6e98398090d2da8d8e14c45d0bda2d9bb6bfd27
hebohao <hebohao@bfops.cn>
2021-06-04 09:37:04 +0800

    160

--- 160. 相交链表.py
+++ 160. 相交链表.py
@@ -19,3 +19,41 @@ class Solution:
             else:
                 HB = headA
         return HA
+
+
+class Solution1:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        tmp = dict()
+        while headA:
+            tmp[headA] = True
+            headA = headA.next
+        while headB:
+            if headB in tmp:
+                return headB
+            headB = headB.next
+        return None
+
+
+c = ListNode(8)
+d = ListNode(4)
+e = ListNode(5)
+
+headA = ListNode(4)
+a2 = ListNode(1)
+headA.next = a2
+headA.next.next = c
+headA.next.next.next = d
+headA.next.next.next.next = 3
+
+b2 = ListNode(6)
+b3 = ListNode(1)
+headB = ListNode(5)
+headB.next = b2
+headB.next.next = b3
+headB.next.next.next = c
+headB.next.next.next.next = d
+headB.next.next.next.next = e
+
+s = Solution()
+r = s.getIntersectionNode(headA, headB)
+print(r.val)
